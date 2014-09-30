e73e52a1ad1bdc92cf74deae35a3b125e1487190
markgarciait <mark.garcia@buchanangroup.com>
2014-09-30 17:42:25 +0800

    add classtest

---
+++ ClassTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    public class Class1
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
